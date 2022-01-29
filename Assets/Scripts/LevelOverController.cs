b8bf59b1ad945c835aaaa63bc3ca8a63b2d899dc
CHAHAT GOEL <chahatgoel2002@gmail.com>
2022-01-30 00:55:18 +0530

    Level Setup

---
+++ Assets/Scripts/LevelOverController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOverController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if(collision.gameObject.CompareTag("Player 3"))
+        if (collision.gameObject.GetComponent<Playercontroller>() != null)
+        {
+            Debug.Log("Level is finished by player");
+        }
+    }
+
+}
